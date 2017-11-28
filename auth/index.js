cda113c82c0a734e9341638ef3fec558b64556b3
alexanderpaulino <apaulino127@gmail.com>
2017-11-28 15:14:38 -0500

    Reverting some code in passport authentication

--- auth/index.js
+++ auth/index.js
@@ -28,16 +28,13 @@ router.post('/login', (req, res, next) => {
 		console.log('========Pre-Authentication=========')
 		next()
 	},
-	passport.authenticate('local'), (err, req, res) => {
+	passport.authenticate('local'), (req, res) => {
 		console.log('POST to /login')
 		const user = JSON.parse(JSON.stringify(req.user)) // hack
 		const cleanUser = Object.assign({}, user)
 		if (cleanUser.local) {
 			console.log(`Deleting ${cleanUser.local.password}`)
 			delete cleanUser.local.password
-		}
-		if (err) {
-			return (res.json(err))
 		}
 		res.json({ user: cleanUser })
 	}
